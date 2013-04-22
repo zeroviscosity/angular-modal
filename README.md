12f6215865dfe8091055ba21a5696714b564e9a2
Kent English <kent.english@gmail.com>
2013-04-21 20:26:00 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-angular-modal
-=============
+# Angular Modal
+
+### An AngularJS Directive for controlling a Bootstrap Modal
+
+### Usage
+
+* Replace `myApp` with the name of your AngularJS application 
+* Place the following include your base controller:
+
+    <div angular-modal></div>
+    
+* To activate the modal, broadcast a `showModal` from the $rootScope along with
+  modal options:
+
+    $rootScope.$broadcast('showModal', {
+        title: 'Error', 
+        message: 'Some important error message!', 
+        alert: 'error'
+    });
+    
---
+++ angular-modal.js
@@ -0,0 +1,53 @@
+/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+* Angular Modal
+* 
+* Copyright (C) 2013 Kent English (Zero Viscosity)
+*
+* License: MIT
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+
+myApp.directive('angularModal', function() {
+    return {
+        controller: ['$scope', '$rootScope', '$element', 
+            function($scope, $rootScope, $element) {
+                $scope.defaults = {
+                    title: 'Notification',
+                    class: '',
+                    alert: null,
+                    message: '',
+                    action: function() {  }
+                };
+                $scope.options = {};
+                
+                $rootScope.$on('showModal', function(evt, options) {
+                    //Reset the options
+                    $scope.options = angular.copy($scope.defaults);
+                    //Include new options
+                    angular.extend($scope.options, options);
+                    $scope.options.class = ($scope.options.alert) ? 
+                            'alert alert-' + $scope.options.alert : '';
+                    //Display the modal
+                    $($element).find('.modal').modal('show');
+                });
+            }
+        ],
+        template: 
+            '<div class="modal hide fade">' +
+            '  <div class="modal-header">' +
+            '    <button type="button" class="close" data-dismiss="modal" ' +
+            '        aria-hidden="true">&times;</button>' +
+            '    <h3>{{options.title}}</h3>' +
+            '  </div>' +
+            '  <div class="modal-body">' +
+            '    <div ng-class="options.class">' +
+            '      {{options.message}}' +
+            '    </div>' +
+            '  </div>' +
+            '  <div class="modal-footer">' +
+            '    <button class="btn" data-dismiss="modal" aria-hidden="true" ' +
+            '        ng-click="options.action()">OK</button>' +
+            '  </div>' +
+            '</div>'
+    };
+});
+
